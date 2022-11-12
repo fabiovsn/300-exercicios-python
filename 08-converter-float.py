702422906c3fd8be4ad72897de6ecb0b676d8c00
fabio <fabiovsn@hotmail.com>
2022-11-12 14:35:13 -0300

    Exercício 08 - converte número

---
+++ 08-converter-float.py
@@ -0,0 +1,6 @@
+# Converte um número para float
+
+numero = input("Digite um número:\n")
+numero_convertido = float(numero)
+print(f"O número digitado é: {numero_convertido}")
+print(f"O tipo é: {type(numero_convertido)}")
