490ab7d4d1805018fb3cab3b7669958e75264c1b
fabio <fabiovsn@hotmail.com>
2022-11-13 10:00:58 -0300

    Exercício 17 - verifica se valor é maior que outro

---
+++ 17-maior-que.py
@@ -0,0 +1,7 @@
+# Verifica se valor é maior que outro
+
+valor1 = 10
+valor2 = 20
+
+print(f"O valor {valor1} é maior que {valor2}: {valor1 > valor2}")
+print(f"O valor {valor2} é maior que {valor1}: {valor2 > valor1}")
