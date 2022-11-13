e1dbf5a4f1ec89e18357e1e3069955504f2829fa
fabio <fabiovsn@hotmail.com>
2022-11-13 09:57:42 -0300

    Exercício 16 - entrada de dados e operações

---
+++ 16-entrada-de-dados-operacoes.py
@@ -0,0 +1,9 @@
+# Operações matemáticas com entrada de dados
+
+num1 = int(input("Digite um número:\n"))
+num2 = int(input("Digite outro número:\n"))
+
+print(f"A soma de {num1} e {num2} é: {num1 + num2}\n")
+print(f"A subtração de {num1} e {num2} é: {num1 - num2}\n")
+print(f"A multiplicação de {num1} e {num2} é: {num1 * num2}\n")
+print(f"A divisão de {num1} e {num2} é: {num1 / num2}\n")
