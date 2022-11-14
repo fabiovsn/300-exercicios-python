928fd849ccf563ff88eca44d46dc1b6ca1682c89
fabio <fabiovsn@hotmail.com>
2022-11-14 16:51:59 -0300

    Exercício 29 - contagem intervalo

---
+++ 29-contagem-intervalo.py
@@ -0,0 +1,8 @@
+# Ler dois números e imprimir intervalo entre eles
+
+inicio = int(input("Digite o ínicio\n"))
+fim = int(input("Digite o fim\n"))
+
+for i in range(inicio, fim + 1):
+    print(i)
+
