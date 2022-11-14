6efee5ee10e30cf3dc20f35298d3d86287383d61
fabio <fabiovsn@hotmail.com>
2022-11-14 14:04:10 -0300

    Exercício 22 - verifica se valores são iguais ou maiores que 100

---
+++ 22-igual-ou-maiores-100.py
@@ -0,0 +1,9 @@
+# Verifica se valor é igual ou menor
+
+valor1 = 10
+valor2 = 102
+
+print(f"Número {valor1} é maior ou igual a 100: {valor1 >= 100}")
+print(f"Número {valor2} é maior ou igual a 100: {valor2 >= 100}")
+
+print(f"Número {valor1} ou {valor2} são maiores ou iguais a 100: {valor1 >= 100 or valor2 >= 100}")
