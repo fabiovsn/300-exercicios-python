1ed92b72badfd3302c953e2240410aea72c8fd2a
fabio <fabiovsn@hotmail.com>
2022-11-14 14:01:33 -0300

    Exercício 21 - verifica se valor é igual ou menor que 100

--- 21-igual-ou-menor-100.py
+++ 21-igual-ou-menor-100.py
@@ -5,3 +5,5 @@ valor2 = 102
 
 print(f"Número {valor1} é menor ou igual a 100: {valor1 <= 100}")
 print(f"Número {valor2} é menor ou igual a 100: {valor2 <= 100}")
+
+print(f"Número {valor1} e {valor2} são menores e iguais a 100: {valor1 and valor2 <= 100}")
