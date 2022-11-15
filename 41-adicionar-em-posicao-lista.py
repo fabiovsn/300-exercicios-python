d029696e955d1f72ee169a83007ab7379f87dd07
fabio <fabiovsn@hotmail.com>
2022-11-15 20:31:57 -0300

    Exercício 41 - adicionando nome em posição específica da lista

---
+++ 41-adicionar-em-posicao-lista.py
@@ -0,0 +1,10 @@
+# Adicionar nome em posição específica da lista
+
+lista = ["Ana", "Carlos", "Daiane", "Fernando", "Maria"]
+print(lista)
+
+lista.append("Paulo")
+print(lista)
+
+lista.insert(2, "Eliana")
+print(lista)
