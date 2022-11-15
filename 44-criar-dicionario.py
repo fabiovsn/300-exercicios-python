a993d8efe5fcd3cd88d455c0e7b904d1784d7813
fabio <fabiovsn@hotmail.com>
2022-11-15 20:40:13 -0300

    Exercício 44 - criar dicionário com dict

---
+++ 44-criar-dicionario.py
@@ -0,0 +1,8 @@
+# Criar dicionário usando dict
+
+dicionario = dict(Pão="R$ 1,99",
+                  Açucar="R$ 4,99",
+                  Macarrão="R$ 3,99",
+                  Carne="R$ 16,99")
+
+print(dicionario)
