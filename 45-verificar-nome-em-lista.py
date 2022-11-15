67591ed256fe681ce7219113e1524fa136aa6899
fabio <fabiovsn@hotmail.com>
2022-11-15 20:46:43 -0300

    Exercício 45 - verificar nome em lista

---
+++ 45-verificar-nome-em-lista.py
@@ -0,0 +1,17 @@
+# Verificar se nome consta em lista
+
+linguagens = ["C", "Javascript", "Lua", "Python"]
+
+# if "Python" in linguagens:
+#     print("Python consta na lista de linguagens de programação")
+# else:
+#     print("Python não consta na lista de linguagens de programação")
+
+#alternativa
+
+for i in linguagens:
+    if i == "Python":
+        print("Python consta na lista de linguagens de programação")
+    else:
+        # print("Python não consta na lista de linguagens de programação")
+        pass
