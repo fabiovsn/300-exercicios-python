fd89bb2b42be64b6eaee83cae7feab45cd354c5e
fabio <fabiovsn@hotmail.com>
2022-11-15 20:55:42 -0300

    Exercício 47 - alimentar dicionário

---
+++ 47-alimentar-dicionario.py
@@ -0,0 +1,8 @@
+# Alimentar dicionário com valores de duas listas
+
+itens = ["caneta", "lápis", "borracha", "caderno"]
+precos = ["1.99", "0.99", "0.50", "9.90"]
+
+dicionario = dict(keys = itens, values = precos)
+print(dicionario)
+print(type(dicionario))
