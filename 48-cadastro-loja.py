5f0a41b5f083cacf45181cac4a0c0e617595d8f1
fabio <fabiovsn@hotmail.com>
2022-11-15 20:59:46 -0300

    Exercício 48 - cadastro loja dicionário

---
+++ 48-cadastro-loja.py
@@ -0,0 +1,12 @@
+# Cadastro de loja simples
+
+cadastro = {"Nome": "Paulo",
+            "Sexo": "Masculino",
+            "Idade": 38,
+            "Nacionalidade": "Brasileira",
+            "Estado civil": "Divorciado",
+            "Escolaridade": ["Ensino Superior", "Doutorado"],
+            "Ocupação": "Professor",
+            "Renda": "R$ 1999.00 - R$ 2999.00"}
+
+print(cadastro)
