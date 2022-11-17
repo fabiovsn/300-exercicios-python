ae802cfe933efe53aece53fdc023379f76cf3271
fabio <fabiovsn@hotmail.com>
2022-11-16 23:16:40 -0300

    Exercício 59 - funcao com três parâmetros

---
+++ 59-funcao-tres-parametros.py
@@ -0,0 +1,6 @@
+# Função com três parâmetros
+
+def pessoa1(nome, idade=18, funcao="técnico"):
+    print(f"{nome} tem {idade} anos, e sua função é {funcao}")
+
+p1 = pessoa1("Fernando", funcao="gerente")
