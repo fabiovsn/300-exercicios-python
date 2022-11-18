29643dfc82d009e364ace83d217148265f16aaa5
fabio <fabiovsn@hotmail.com>
2022-11-18 15:35:15 -0300

    Exercício 70 - Classe Pessoa com atributos vazios

---
+++ 70-classe-Pessoa-atributos-vazios.py
@@ -0,0 +1,16 @@
+# Classe Pessoa com objetos vazios
+
+class Pessoa:
+    def __init__(self):
+        self.nome = None
+        self.idade = None
+        self.altura = None
+        self.peso = None
+        self.sexo = None
+
+
+
+
+
+
+
