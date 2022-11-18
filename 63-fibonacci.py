1c518c6775088c1817bb73fd3e3e35475ac31d31
fabio <fabiovsn@hotmail.com>
2022-11-17 21:24:06 -0300

    Exercício 63 - sequência de fibonacci

---
+++ 63-fibonacci.py
@@ -0,0 +1,13 @@
+#Sequência de Fibonacci
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+num = int(input("Digite um número para encontrar seu Fibonacci:"))
+resposta = fibonacci(num-1)
+print(resposta)
+
+
