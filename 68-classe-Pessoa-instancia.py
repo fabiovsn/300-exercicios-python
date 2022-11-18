bb1f71623ac1c79ba4c68de03f33515f40bfb862
fabio <fabiovsn@hotmail.com>
2022-11-18 15:25:53 -0300

    Exercício 68 - Classe Pessoa com instância de objeto

---
+++ 68-classe-Pessoa-instancia.py
@@ -0,0 +1,14 @@
+# Classe Pessoa com instância de objeto
+
+class Pessoa:
+    pass
+
+pessoa1 = Pessoa()
+pessoa1.nome = "Fernando"
+pessoa1.idade = 33
+pessoa1.profissao = "Professor"
+
+print(f"{pessoa1.nome}, {pessoa1.idade}, {pessoa1.profissao}")
+
+
+
