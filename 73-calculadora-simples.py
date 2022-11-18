924531a9d9647660ae4bb05aa9e95a941f8c2757
fabio <fabiovsn@hotmail.com>
2022-11-18 15:56:28 -0300

    Exercício 73 - Classe Calculadora

---
+++ 73-calculadora-simples.py
@@ -0,0 +1,26 @@
+# Classe Calculadora
+
+class Calculadora:
+    def soma(self, n1, n2):
+        return n1 + n2
+
+    def subtracao(self, n1, n2):
+        return n1 - n2
+
+    def multiplicacao(self, n1, n2):
+        return n1 * n2
+
+    def divisao(self, n1, n2):
+        return n1 / n2
+
+n1 = Calculadora()
+print(n1.multiplicacao(5, 4))
+
+
+
+
+
+
+
+
+
