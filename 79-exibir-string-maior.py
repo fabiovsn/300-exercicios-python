ff6cdcfb9ca1cd82cef33200d71caf214ea122a3
fabio <fabiovsn@hotmail.com>
2022-11-20 16:55:30 -0300

    Exercício 79 - Exibir string maior ou ambas, se tamanho iguais

---
+++ 79-exibir-string-maior.py
@@ -0,0 +1,16 @@
+# Exibir maior string
+
+def mostra_string_maior(s1, s2):
+    len1 = len(s1)
+    len2 = len(s2)
+
+    if len1 > len2:
+        print(f"{s1} é a string de maior tamanho.")
+    elif len2 > len1:
+        print(f"{s2} é a string de maior tamanho.")
+    else:
+        print(f"{s1} e {s2} são strings de mesmo tamanho.")
+        print(s1)
+        print(s2)
+
+mostra_string_maior("alho", "alho")
