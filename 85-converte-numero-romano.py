8ea3253020c50054a397a86b91e7a7591fd75ee2
fabio <fabiovsn@hotmail.com>
2022-11-22 00:11:14 -0300

    Exercício 85 - Converte número romano

---
+++ 85-converte-numero-romano.py
@@ -0,0 +1,31 @@
+# Converte em número romano
+
+class Conversor:
+    def int_para_romano(self, num):
+        val_int = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+        ]
+
+        val_rom = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+        ]
+
+        numero_romano = ""
+
+        i = 0
+        while num > 0:
+            for _ in range(num//val_int[i]):
+                numero_romano += val_rom[i]
+                num -= val_int[i]
+            i += 1
+
+        return numero_romano
+
+num =int(input("Digite um número a ser convertido:\n"))
+print(Conversor().int_para_romano(num))
