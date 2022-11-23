03ab8b3dffa1ee0ebdea52ab4a5c856d3c4ab47d
fabio <fabiovsn@hotmail.com>
2022-11-22 21:38:14 -0300

    Exercício 90 - Número ao quadrado com documentação

---
+++ 90-numero-ao-quadrado-documentacao.py
@@ -0,0 +1,8 @@
+# Retorna número elevado ao quadrado com documentação
+
+def ao_quadrado(num):
+    """Esta função recebe do usuário um número qualquer e retorna esse número
+    elevado ao quadrado. Ex.: Recebe 8 e retorna 64."""
+    return num ** 2
+
+print(ao_quadrado.__doc__)
