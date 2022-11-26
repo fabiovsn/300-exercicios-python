1ca1a088e40580463e1ad452e807ee82ed4f80ce
fabio <fabiovsn@hotmail.com>
2022-11-26 17:59:23 -0300

    Exercício 105 - orientação a objetos híbrida

---
+++ 105-numeros-ordenados.py
@@ -0,0 +1,13 @@
+# 50 elementos ordenados de forma crescente
+
+lista1 = list()
+
+for num in range(51):
+    lista1.append(num)
+
+print(lista1)
+
+# alternativa
+
+lista2 = [num for num in range(51)]
+print(lista2)
