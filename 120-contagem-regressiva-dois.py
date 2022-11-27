021f4051fe860e771c2184096180bde426792cbe
fabio <fabiovsn@hotmail.com>
2022-11-27 17:02:42 -0300

    Exercício 120 - Contagem regressiva de dois segundos

---
+++ 120-contagem-regressiva-dois.py
@@ -0,0 +1,18 @@
+# Contagem regressiva de dois em dois
+
+import time
+
+final = 0
+total = 10
+
+print("Contagem iniciada...")
+while final <= 10:
+    print(total)
+    time.sleep(2)
+    total -= 1
+    final = total
+
+
+    if total == -1:
+        print("Contagem encerrada!")
+        break
