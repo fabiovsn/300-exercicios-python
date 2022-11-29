0cf4c9cd668b5c03b41b11ae3a12a5a180e95c9a
fabio <fabiovsn@hotmail.com>
2022-11-29 16:13:44 -0300

    Exercício 141 - Função inventário

---
+++ 141-funcao-inventario.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+
+def inventario(item, localizacao, quantidade, preco, categoria):
+    pass
+
+# Método discriminado
+
+def inventario(item = 0,
+               localizacao = "Estoque",
+               quantidade = 0,
+               preco:float = 0.01,
+               categoria = list['Perifericos',
+               'Acessorios',
+               'Outros'],
+               **kwargs)->np.ndarray:
+    pass
