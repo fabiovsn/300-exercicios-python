71a551bb77cc582f9583acee671120d209105781
fabio <fabiovsn@hotmail.com>
2022-11-29 16:21:49 -0300

    Exercício 143 - Contagem regressiva

---
+++ 143-contagem-regressiva.py
@@ -0,0 +1,15 @@
+import time
+
+final = 0
+total = 10
+
+print("Contagem regressiva...")
+
+while final <= 10:
+    print(total)
+    time.sleep(2)
+    total -= 1
+    final = total
+
+    if total == -1:
+        print("Contagem encerrada!")
