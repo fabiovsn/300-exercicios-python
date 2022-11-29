8e6fa3b726cdc6f1f02db69e9eaceb0214c1ac6c
fabio <fabiovsn@hotmail.com>
2022-11-29 11:29:01 -0300

    Exercício 139 - Quantidade de letras maiúsculas e minúsculas

---
+++ 139-quantas-maiusculas-minusculas.py
@@ -0,0 +1,16 @@
+texto = input("Digite um texto:\n")
+
+maiusculas = []
+minusculas = []
+
+for letra in texto:
+    if letra.islower():
+        minusculas.append(letra)
+    elif letra.isupper():
+        maiusculas.append(letra)
+
+print(f"No texto {texto} foram encontradas as seguintes letras maiúsculas: {maiusculas}")
+print(f"No texto {texto} foram encontradas {len(maiusculas)} as seguintes letras maiúsculas")
+
+print(f"No texto {texto} foram encontradas as seguintes letras minúsculas: {minusculas}")
+print(f"No texto {texto} foram encontradas {len(minusculas)} as seguintes letras minúsculas")
