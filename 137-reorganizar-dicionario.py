29922eaa3f6ebc8161fcb948e013071d4e7c78a8
fabio <fabiovsn@hotmail.com>
2022-11-29 10:57:13 -0300

    Exercício 137 - Reorganizar dicionários

---
+++ 137-reorganizar-dicionario.py
@@ -0,0 +1,16 @@
+# Reorganizar carros em dicionário de acordo com ano de fabricação
+
+def f(crit):
+    return crit['ano']
+
+carros=[
+    {'carro': 'Celta', 'ano': 2005},
+    {'carro': 'Gol', 'ano': 2000},
+    {'carro': 'Palio', 'ano': 2019},
+    {'carro': 'Corsa', 'ano': 2011},
+    {'carro': 'Fiesta', 'ano': 2009}
+]
+
+carros.sort(key=f)
+
+print(carros)
