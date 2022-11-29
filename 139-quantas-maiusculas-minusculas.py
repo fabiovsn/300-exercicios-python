07a46f9ee574218fe9c818f88a57b11d9392fd7e
fabio <fabiovsn@hotmail.com>
2022-11-29 11:31:53 -0300

    Exercício 139 - Quantidade de letras maiúsculas e minúsculas

--- 139-quantas-maiusculas-minusculas.py
+++ 139-quantas-maiusculas-minusculas.py
@@ -1,16 +1,28 @@
-texto = input("Digite um texto:\n")
+# texto = input("Digite um texto:\n")
+#
+# maiusculas = []
+# minusculas = []
+#
+# for letra in texto:
+#     if letra.islower():
+#         minusculas.append(letra)
+#     elif letra.isupper():
+#         maiusculas.append(letra)
+#
+# print(f"No texto {texto} foram encontradas as seguintes letras maiúsculas: {maiusculas}")
+# print(f"No texto {texto} foram encontradas {len(maiusculas)} as seguintes letras maiúsculas")
+#
+# print(f"No texto {texto} foram encontradas as seguintes letras minúsculas: {minusculas}")
+# print(f"No texto {texto} foram encontradas {len(minusculas)} as seguintes letras minúsculas")
 
-maiusculas = []
-minusculas = []
+# Método reduzido
 
-for letra in texto:
-    if letra.islower():
-        minusculas.append(letra)
-    elif letra.isupper():
-        maiusculas.append(letra)
+texto = input("Digite o texto:\n")
+
+minusculas = [letra for letra in texto if letra.islower()]
+maiusculas = [letra for letra in texto if letra.isupper()]
 
 print(f"No texto {texto} foram encontradas as seguintes letras maiúsculas: {maiusculas}")
 print(f"No texto {texto} foram encontradas {len(maiusculas)} as seguintes letras maiúsculas")
-
 print(f"No texto {texto} foram encontradas as seguintes letras minúsculas: {minusculas}")
 print(f"No texto {texto} foram encontradas {len(minusculas)} as seguintes letras minúsculas")
