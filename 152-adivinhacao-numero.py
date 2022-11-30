9bdb62a11c0cbd269135d642c439649588903ad0
fabio <fabiovsn@hotmail.com>
2022-11-30 14:46:48 -0300

    Exercício 152 - Adivinhar o número

---
+++ 152-adivinhacao-numero.py
@@ -0,0 +1,11 @@
+# Adivinhação de número
+
+import random
+
+num_gerado, num_adivinhado = random.randint(0, 11), 0
+
+while num_adivinhado != num_gerado:
+    num_adivinhado = int(input("Digite um número entre 0 e 10:\n"))
+
+print("Parabéns, você adivinhou o número!!")
+print(f"Número gerado: {num_gerado}")
