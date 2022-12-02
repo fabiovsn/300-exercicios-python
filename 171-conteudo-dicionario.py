ca5b9ac4f0c72d2852ac8e1f8c712e7240de2ffe
fabio <fabiovsn@hotmail.com>
2022-12-02 10:51:30 -0300

    Exercício 171 - Conteúdo de dicionário

---
+++ 171-conteudo-dicionario.py
@@ -0,0 +1,20 @@
+# Conteúdo de dicionário
+
+from pprint import pprint
+
+data = {
+    "usuarios":[{"Id":1,
+                 "Nome":"Rafael",
+                 "Sobrenome":"Moraes",
+                 "Email":"moraes@gmail.com"},
+                {"Id":2,
+                 "Nome":"Paula",
+                 "Sobrenome":"Santos",
+                 "Email":"paulasas@hotmail.com"},
+                {"Id":3,
+                 "Nome":"Daniel",
+                 "Sobrenome":"Pires",
+                 "Email":"dr.daniel@gmail.com"}]}
+
+pprint(data)
+print(data)
