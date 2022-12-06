416cb660ca152851e2ed5b61887418081c667674
fabio <fabiovsn@hotmail.com>
2022-12-05 23:23:38 -0300

    Exercício 24 - Numpy - Array maior

---
+++ 24-numpy-arrray-maior.py
@@ -0,0 +1,11 @@
+# Retorno True para cada não numérico
+
+import numpy as np
+
+import numpy as np
+
+A = np.array([0.4, 0.5])
+B = np.array([0.39999999, 0.5000001])
+
+print(A > B)
+print(np.greater(A, B))
