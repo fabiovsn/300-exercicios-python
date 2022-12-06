84f4fa7386e76fb0ab7cea454128495cc0d9218b
fabio <fabiovsn@hotmail.com>
2022-12-06 18:10:20 -0300

    Exercício 34 - Numpy - Inserção de colunas e linhas

---
+++ 34-numpy-inserir-linhas-colunas.py
@@ -0,0 +1,11 @@
+# Array de números sequenciais
+
+import numpy as np
+
+import numpy as np
+
+A = np.ones(shape = (4, 4))
+print(A)
+
+print(np.pad(A, pad_width=1,
+             constant_values=0))
