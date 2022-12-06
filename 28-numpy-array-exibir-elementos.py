e365d9c10777f572869d4e427fc6f1ce1cd966e3
fabio <fabiovsn@hotmail.com>
2022-12-06 17:37:57 -0300

    Exercício 28 - Numpy - Array exibir elementos

---
+++ 28-numpy-array-exibir-elementos.py
@@ -0,0 +1,11 @@
+# Array de números aleatórios
+
+import numpy as np
+
+import numpy as np
+
+array = np.array([[1, 4, 3, 7],
+                  [5, 2, 6, 8]])
+
+for i in np.nditer(array):
+    print(i)
