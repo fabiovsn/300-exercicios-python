762ba6d2193a00657ad80743eaa7d2cb55b99249
fabio <fabiovsn@hotmail.com>
2022-12-06 17:56:47 -0300

    Exercício 32 - Numpy - Array para lista

---
+++ 32-numpy-array-lista.py
@@ -0,0 +1,14 @@
+# Array de números sequenciais
+
+import numpy as np
+
+import numpy as np
+
+A = np.arange(12, dtype = "int").reshape(-1 , 4)
+lista = A.tolist()
+
+print(A)
+print(lista)
+
+print(type(A))
+print(type(lista))
