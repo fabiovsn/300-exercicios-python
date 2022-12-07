b341fff6ad6eb4d567a725099726cf4fbe963096
fabio <fabiovsn@hotmail.com>
2022-12-07 19:59:03 -0300

    Exercício 36 - Numpy - Números aleatórios em ordem

---
+++ 36-numpy-numeros-aleatorios-ordem.py
@@ -0,0 +1,12 @@
+# Array de números aleatórios em ordem
+
+import numpy as np
+
+import numpy as np
+
+A = np.random.randint(low = 1,
+                      high = 10,
+                      size = (2, 3))
+
+print(A)
+print(np.unique(A))
