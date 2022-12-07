da54cfa2ff20e17672e891ec814e10b3ee2aabcd
fabio <fabiovsn@hotmail.com>
2022-12-07 20:48:42 -0300

    Exercício 45 - Numpy - Valores próprios

---
+++ 45-numpy-valores-proprios.py
@@ -0,0 +1,11 @@
+# Elemento de maior valor
+
+import numpy as np
+
+import numpy as np
+
+A = np.array([[[4, 5, 4, 2], [6, 3, 5, 1], [5, 2, 1, 2]],
+              [[7, 2, 3, 1], [6, 2, 0, 9], [0, 1, 9, 1]]])
+
+print(A)
+print(A.shape)
