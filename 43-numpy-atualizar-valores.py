b5f630437a7fcbc953df60de5cc1022ec77b5215
fabio <fabiovsn@hotmail.com>
2022-12-07 20:42:59 -0300

    Exercício 43 - Numpy - Atualizar valores

---
+++ 43-numpy-atualizar-valores.py
@@ -0,0 +1,11 @@
+# Elemento de maior valor
+
+import numpy as np
+
+import numpy as np
+
+array = np.array([[4.99, 3.49, 9.99],
+              [1.99, 9.99, 14.99],
+              [14.99, 2.39, 7.29]])
+
+print(np.full_like(array, 5.5))
