3be7798e449984e702dc94ad3eee8a1de44bdfd7
fabio <fabiovsn@hotmail.com>
2022-12-07 20:33:04 -0300

    Exercício 39 - Numpy - Exibir números maiores que 8

---
+++ 39-numpy-maior-oito.py
@@ -0,0 +1,11 @@
+# Elemento de maior valor
+
+import numpy as np
+
+import numpy as np
+
+A = np.array([[4.99, 3.49, 9.99],
+              [1.99, 8.99, 4.99],
+              [14.99, 2.39, 7.29]])
+
+print(A[A > 8])
