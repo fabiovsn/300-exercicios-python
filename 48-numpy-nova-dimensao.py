44f580fba30779b68075829abae4318ac9b0673f
fabio <fabiovsn@hotmail.com>
2022-12-08 18:40:56 -0300

    Exercício 48 - Numpy - Nova dimensão

---
+++ 48-numpy-nova-dimensao.py
@@ -0,0 +1,16 @@
+# Elemento de maior valor
+
+import numpy as np
+
+import numpy as np
+
+array = np.array([[4, 2, 1],
+                  [6, 4, 2]])
+
+print(array)
+print(array.shape)
+
+array = np.expand_dims(array, axis=0)
+
+print(array)
+print(array.shape)
