001518c5e04f28c3c97686b6b3bf095661208419
fabio <fabiovsn@hotmail.com>
2022-12-08 18:35:19 -0300

    Exercício 46 - Numpy - Imagem ruído

---
+++ 46-numpy-imagem-ruido.py
@@ -0,0 +1,12 @@
+# Elemento de maior valor
+
+import numpy as np
+
+import numpy as np
+
+imagem = np.random.randint(low=0,
+                           high=256,
+                           size=(800, 600),
+                           dtype=np.uint8)
+
+print(imagem)
