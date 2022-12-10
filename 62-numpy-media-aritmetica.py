b55e66ed49d70fdb3f3c210fe4a2bda0f986e58b
fabio <fabiovsn@hotmail.com>
2022-12-10 14:06:11 -0300

    Exercício 62 - Numpy - Média aritmética

---
+++ 62-numpy-media-aritmetica.py
@@ -0,0 +1,14 @@
+# Definir casas decimais como 5
+
+import numpy as np
+
+import numpy as np
+
+A = np.array([[3, 4, 9, 2],
+              [5, 3, 2, 5]])
+
+B = np.array([[4, 3, 2, 5],
+              [6, 3, 1, 6]])
+
+media = np.divide(np.add(A, B), 2)
+print(media)
