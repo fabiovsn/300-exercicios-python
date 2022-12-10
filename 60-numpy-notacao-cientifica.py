da2380df4efc40e5acbebe2a71937b0190f1e5df
fabio <fabiovsn@hotmail.com>
2022-12-10 14:00:11 -0300

    Exercício 60 - Numpy - Notação científica

---
+++ 60-numpy-notacao-cientifica.py
@@ -0,0 +1,11 @@
+# Definir casas decimais como 5
+
+import numpy as np
+
+import numpy as np
+
+array = np.array([1.2e-6, 1.7e-7])
+np.set_printoptions(suppress=True,
+                    precision=8)
+
+print(array)
