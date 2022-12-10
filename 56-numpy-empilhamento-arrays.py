75c9c4b23ccfa6a3f807c1608bc21402b33abe2f
fabio <fabiovsn@hotmail.com>
2022-12-10 13:45:10 -0300

    Exercício 56 - Numpy - Empilhamento de arrays

---
+++ 56-numpy-empilhamento-arrays.py
@@ -0,0 +1,13 @@
+# Nova imagem com cinco primeiras linhas e colunas
+
+import numpy as np
+
+import numpy as np
+
+a1 = np.array([1.6, 0.9, 2.2])
+a2 = np.array([0.4, 1.3, 3.2])
+a3 = np.array([1.4, 0.3, 1.2])
+
+pilha = np.column_stack((a1, a2, a3))
+
+print(pilha)
