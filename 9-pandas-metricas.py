3af5a1ed0afd25d7066ccf32d3f9fecca365b1d3
fabio <fabiovsn@hotmail.com>
2022-12-11 10:45:54 -0300

    Exercício 9 - Pandas - Métricas diversas

---
+++ 9-pandas-metricas.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+estoque = {'Cel Xiaomi Redmi Note 11': 1699,
+           'Cel Samsung Galaxy s22': 3999,
+           'Cel Motorola Moto G200': 2999,
+           'Cel LG V60 ThinQ': 1999}
+
+pdestoque = pd.Series(data = estoque)
+pdestoque = pd.DataFrame(pdestoque, columns=['Preço'])
+
+print(pdestoque.describe())
