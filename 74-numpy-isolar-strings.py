9f7517c3172a18c380f34a325f2ef2a5aaa16bc3
fabio <fabiovsn@hotmail.com>
2022-12-11 09:56:37 -0300

    Exercício 74 - Numpy - Isolar strings

---
+++ 74-numpy-isolar-strings.py
@@ -0,0 +1,10 @@
+# Definir casas decimais como 5
+
+import numpy as np
+
+import numpy as np
+
+A = np.array([['PLW CDR 11B TEN', 'AMC LPP'],
+              ['CDR PKO KGH', 'CCC QMK']], dtype=str)
+
+print(np.char.split(A))
