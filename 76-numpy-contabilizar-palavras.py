9581751d4968e2dadf065377aba586e1eeff1ed9
fabio <fabiovsn@hotmail.com>
2022-12-11 10:04:04 -0300

    Exercício 76 - Numpy - Contabilizar palavras

---
+++ 76-numpy-contabilizar-palavras.py
@@ -0,0 +1,11 @@
+# Definir casas decimais como 5
+
+import numpy as np
+
+import numpy as np
+
+array = np.array([[' #music #rock #metal', '#metal #poprock'],
+              ['# rock #music', '#music #metal'],
+              ['#rock #metal #heavymetal', '#deathmetal #metal']])
+
+print(np.char.count(array, 'metal'))
