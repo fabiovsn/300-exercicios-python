c78c39f8dd381ecf75f31586be56f486271f1174
fabio <fabiovsn@hotmail.com>
2022-12-11 09:59:42 -0300

    Exercício 75 - Numpy - Remover caracteres

---
+++ 75-numpy-remover-caracteres.py
@@ -0,0 +1,10 @@
+# Definir casas decimais como 5
+
+import numpy as np
+
+import numpy as np
+
+A = np.array([[' #Steam#Games'],
+              ['# #Youtube#videos']])
+
+print(np.char.strip(np.char.replace(A, '#', ' ')))
