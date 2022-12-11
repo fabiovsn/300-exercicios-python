55b149042ebc48af1d4b3fdaa8d6bab78f7cda10
fabio <fabiovsn@hotmail.com>
2022-12-11 11:04:18 -0300

    Exercício 14 - Pandas - Maior e menor valores

---
+++ 14-pandas-menor-e-maior.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+estoque = {'Cel Xiaomi Redmi Note 11': 1699,
+           'Cel Samsung Galaxy s22': 3999,
+           'Cel Motorola Moto G200': 2999,
+           'Cel LG V60 ThinQ': 1999}
+
+pdestoque = pd.Series(data = estoque)
+pdestoque = pd.DataFrame(pdestoque).reset_index()
+pdestoque.columns = ['Item', 'Preço']
+
+precos = pdestoque['Preço']
+
+print(precos)
+print(f'Maior valor encontrado: R$ {precos[:6].max()},00')
+print(f'Menor valor encontrado: R$ {precos[:6].min()},00')
