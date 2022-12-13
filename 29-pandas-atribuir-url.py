5cb686fb7edf9a3a592d2c2ad67a4b760d15e7f9
fabio <fabiovsn@hotmail.com>
2022-12-13 10:16:49 -0300

    Exercício 29 - Pandas - Atribuir url

---
+++ 29-pandas-atribuir-url.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+url = ("https://ml-repository-krakers.s3-eu-west-1.amazonaws.com/kaggle+/churn_modelling/Telco-Customer-Churn.csv")
+
+df = pd.read_csv(url)
+
+print(df.head())
+print(type(df))
