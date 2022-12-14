4baff118fe579760b78bc59d6e4b5a9680f7c419
fabio <fabiovsn@hotmail.com>
2022-12-14 08:00:11 -0300

    Exercício 35 - Pandas - Modificar notação

---
+++ 35-pandas-modificar-notacao.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+url = ("https://ml-repository-krakers.s3-eu-west-1.amazonaws.com/kaggle+/churn_modelling/Telco-Customer-Churn.csv")
+
+df = pd.read_csv(url)
+
+print(df.columns)
+
+df.drop('OnlineBackup', inplace=True, axis=1)
+df.drop('OnlineSecurity', inplace=True, axis=1)
+df.drop('TechSupport', inplace=True, axis=1)
+df.drop('MultipleLines', inplace=True, axis=1)
+df.drop('DeviceProtection', inplace=True, axis=1)
+df.drop('PaperlessBilling', inplace=True, axis=1)
+df.drop('PaymentMethod', inplace=True, axis=1)
+
+media = df['TotalCharges'][df['TotalCharges']!=' '].median()
+df.loc[df['TotalCharges'] == ' ', 'TotalCharges'] = media
+df['TotalCharges'] = df['TotalCharges'].astype('float')
+
+print(df['TotalCharges'].value_counts())
